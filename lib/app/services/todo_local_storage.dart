3ccb8494258a3449058d88fba781cbb4b78a5fc7
Medeiros <hsymetry@gmail.com>
2023-08-24 13:22:03 -0300

    Add done todos local storage

--- lib/app/services/todo_local_storage.dart
+++ lib/app/services/todo_local_storage.dart
@@ -9,7 +9,8 @@ import '../models/todo_model.dart';
 
 class TodoLocalStorage {
   final LocalStorage _localStorage;
-  final String todosKey = "todos";
+  final String todosKey = "todosKey";
+  final String doneTodosKey = "doneTodosKey";
 
   TodoLocalStorage(this._localStorage);
 
@@ -24,8 +25,8 @@ class TodoLocalStorage {
       return null;
     } on TodoErrorMessage {
       return "Erro ao salvar tarefas";
-    } catch (e) {
-      log('Erro ao salvar ToDo\'s');
+    } catch (e, s) {
+      log('Erro ao salvar ToDo\'s', error: e, stackTrace: s);
       return defaultErrorMessage;
     }
   }
@@ -45,11 +46,44 @@ class TodoLocalStorage {
       }
 
       return (null, <TodoModel>[]);
-      
     } on TodoErrorMessage {
       return ("Erro ao salvar tarefas", null);
-    } catch (e) {
-      log('Erro ao salvar ToDo\'s');
+    } catch (e, s) {
+      log('Erro ao salvar ToDo\'s', error: e, stackTrace: s);
+      return (defaultErrorMessage, null);
+    }
+  }
+
+  Future<String?> setDoneTodos(List<String> doneTodos) async {
+    try {
+      final String data = jsonEncode(doneTodos);
+
+      await _localStorage.set(doneTodosKey, data);
+
+      return null;
+    } on TodoErrorMessage {
+      return "Erro ao salvar tarefas prontas";
+    } catch (e, s) {
+      log('Erro ao salvar terefas prontas', error: e, stackTrace: s);
+      return defaultErrorMessage;
+    }
+  }
+
+  Future<(String? error, List<String>? todos)> getDoneTodos() async {
+    try {
+      final String? doneTodosJson = await _localStorage.get(doneTodosKey);
+
+      if (doneTodosJson != null) {
+        final doneTodos = (jsonDecode(doneTodosJson) as List<String>);
+
+        return (null, doneTodos);
+      }
+
+      return (null, <String>[]);
+    } on TodoErrorMessage {
+      return ("Erro ao salvar tarefas prontas", null);
+    } catch (e, s) {
+      log('Erro ao salvar tarefas prontas', error: e, stackTrace: s);
       return (defaultErrorMessage, null);
     }
   }
