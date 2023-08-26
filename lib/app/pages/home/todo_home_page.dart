fb71131fcfdef32ab8744af0abd1d8a4a100b191
Medeiros <hsymetry@gmail.com>
2023-08-25 23:45:17 -0300

    Add provider and create methods to load todo's

--- lib/app/pages/home/todo_home_page.dart
+++ lib/app/pages/home/todo_home_page.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/controllers/todo_controller.dart';
 import 'package:todo_list_provider/app/core/widgets/todo_checkbox.dart';
 import 'package:todo_list_provider/app/core/widgets/todo_texts.dart';
 
-import '../../models/todo_model.dart';
-
-class TodoHomePage extends StatelessWidget {
+class TodoHomePage extends StatefulWidget {
   const TodoHomePage({super.key});
 
   @override
+  State<TodoHomePage> createState() => _TodoHomePageState();
+}
+
+class _TodoHomePageState extends State<TodoHomePage> {
+  bool isLoading = true;
+  String? hasError;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      loadAllTodos();
+    });
+    super.initState();
+  }
+
+  Future loadAllTodos() async {
+    isLoading = true;
+    hasError = null;
+    final controller = context.read<TodoController>();
+
+    final String? errorLoadingTodos = await controller.loadTodos();
+//Caso não carregue, substitua pela call da instancia de controller
+    final String? errorLoadingDoneTodos = await controller.loadDoneTodos();
+
+    if (errorLoadingTodos != null || errorLoadingDoneTodos != null) {
+      setState(() {
+        hasError = errorLoadingTodos ?? errorLoadingDoneTodos;
+      });
+    }
+    setState(() {
+      isLoading = false;
+        
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final List<TodoModel> todos = [
-      TodoModel(title: 'Todo 1', description: 'Primeiro teste'),
-      TodoModel(title: 'Todo 2', description: 'Segundo teste'),
-    ];
+    final controller = context.watch<TodoController>();
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -21,16 +54,24 @@ class TodoHomePage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          final TodoModel todo = todos[index];
-          return ListTile(
-            leading: TodoCheckbox(todo: todo),
-            title: TodoTexts(text: todo.title, fontSize: 18),
-            subtitle: TodoTexts(text: todo.description ?? '', fontSize: 14),
-            trailing:
-                TodoTexts(text: todo.date.toIso8601String(), fontSize: 14),
+      body: Consumer(
+        builder: (context, value, child) {
+          return FutureBuilder(
+            future: controller.loadTodos(),
+            builder: (context, snapshot) => ListView.builder(
+              itemCount: controller.todoList.length,
+              itemBuilder: (context, index) {
+                final todo = controller.todoList[index];
+                return ListTile(
+                  leading: TodoCheckbox(todo: todo),
+                  title: TodoTexts(text: todo.title, fontSize: 18),
+                  subtitle:
+                      TodoTexts(text: todo.description ?? '', fontSize: 14),
+                  trailing: TodoTexts(
+                      text: todo.date.toIso8601String(), fontSize: 14),
+                );
+              },
+            ),
           );
         },
       ),
--- lib/app/todo_list_provider.dart
+++ lib/app/todo_list_provider.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/controllers/todo_controller.dart';
 import 'package:todo_list_provider/app/pages/home/todo_home_page.dart';
+import 'package:todo_list_provider/app/services/local_storage.dart';
+import 'package:todo_list_provider/app/services/todo_local_storage.dart';
 
 import 'core/styles/theme_style.dart';
 
@@ -9,13 +13,19 @@ class TodoListProvider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ToDo List',
-      theme: ThemeStyle.theme,
-      home: const TodoHomePage(),
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [GlobalMaterialLocalizations.delegate],
-      supportedLocales: const [Locale('pt', 'BR'), Locale('en', 'US')],
+    return ChangeNotifierProvider(
+      create: (_) => TodoController(TodoLocalStorage(LocalStorage())),
+      child: MaterialApp(
+        title: 'ToDo List',
+        theme: ThemeStyle.theme,
+        home: const TodoHomePage(),
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+        supportedLocales: const [Locale('pt', 'BR'), Locale('en', 'US')],
+      ),
     );
   }
 }
