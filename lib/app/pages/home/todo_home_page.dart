e7698e765ac8fb6ceacedbdaa718c9b7e67c06cc
Medeiros <hsymetry@gmail.com>
2023-08-28 19:13:39 -0300

    Remove consumer and add formatted date

--- lib/app/pages/home/todo_home_page.dart
+++ lib/app/pages/home/todo_home_page.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:skeletons/skeletons.dart';
 import 'package:todo_list_provider/app/controllers/todo_controller.dart';
+import 'package:todo_list_provider/app/core/styles/colors_style.dart';
 import 'package:todo_list_provider/app/core/widgets/todo_checkbox.dart';
 import 'package:todo_list_provider/app/core/widgets/todo_texts.dart';
-import 'package:todo_list_provider/app/pages/form/add_todo_screen.dart';
+import 'package:todo_list_provider/app/pages/add_todo/add_todo_screen.dart';
 
 class TodoHomePage extends StatefulWidget {
   const TodoHomePage({super.key});
@@ -49,35 +50,37 @@ class _TodoHomePageState extends State<TodoHomePage> {
     final controller = context.watch<TodoController>();
     return SafeArea(
       child: Scaffold(
-        body: Consumer(
-          builder: (context, value, child) {
-            return isLoading
-                ? SkeletonListView()
-                : controller.todoList.isEmpty
-                    ? Center(
-                        child: TodoTexts(
-                            text: hasError != null
-                                ? 'Erro ao carregar tarefas'
-                                : 'Você ainda não criou nenhuma tarefa',
-                            fontSize: 18),
-                      )
-                    : ListView.builder(
-                        itemCount: controller.todoList.length,
-                        itemBuilder: (context, index) {
-                          final todo = controller.todoList[index];
-                          return ListTile(
-                            leading: TodoCheckbox(todo: todo),
-                            title: TodoTexts(text: todo.title, fontSize: 18),
-                            subtitle: TodoTexts(
-                                text: todo.description ?? '', fontSize: 14),
-                            trailing: TodoTexts(
-                                text: todo.date.toIso8601String(),
-                                fontSize: 14),
-                          );
-                        },
-                      );
-          },
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          iconTheme: const IconThemeData(size: 25),
+          title: const TodoTexts(text: 'Lista de afazeres', fontSize: 22),
+          centerTitle: true,
         ),
+        body: isLoading
+            ? SkeletonListView()
+            : controller.todoList.isEmpty
+                ? Center(
+                    child: TodoTexts(
+                        text: hasError != null
+                            ? 'Erro ao carregar lista'
+                            : 'Você não possui nenhuma tarefa',
+                        fontSize: 18),
+                  )
+                : ListView.builder(
+                    itemCount: controller.todoList.length,
+                    itemBuilder: (context, index) {
+                      final todo = controller.todoList[index];
+                      final dateFormatted =
+                          "${todo.date.day.toString()}/${todo.date.month.toString().padLeft(2, '0')}/${todo.date.year.toString().padLeft(2, '0')}";
+                      return ListTile(
+                        leading: TodoCheckbox(todo: todo),
+                        title: TodoTexts(text: todo.title, fontSize: 18),
+                        subtitle: TodoTexts(
+                            text: todo.description ?? '', fontSize: 14),
+                        trailing: TodoTexts(text: dateFormatted, fontSize: 14),
+                      );
+                    },
+                  ),
         floatingActionButton: FloatingActionButton.extended(
           onPressed: hasError != null
               ? loadAllTodos
@@ -86,9 +89,10 @@ class _TodoHomePageState extends State<TodoHomePage> {
                       builder: (context) => const AddTodoScreen(),
                     ),
                   ),
-          label: TodoTexts(
-              text: hasError != null ? 'Recarregar' : 'Nova Tarefa',
-              fontSize: 14),
+          label: Text(
+            hasError != null ? 'Recarregar' : 'Nova Tarefa',
+            style: TextStyle(color: context.colors.primary, fontSize: 15),
+          ),
         ),
       ),
     );
